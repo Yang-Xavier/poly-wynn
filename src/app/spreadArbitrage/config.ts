5df96c8d676444acc992be8a66f02e8e64b09590
Xavier <xavierybx@gmail.com>
2026-01-03 23:30:21 +0800

    chore: change config

--- src/app/spreadArbitrage/config.ts
+++ src/app/spreadArbitrage/config.ts
@@ -6,7 +6,7 @@ const config = {
   delayToStart: 3 * 60 * 1000, // 延迟开始时间
   buyingAmountFactor: 0.2, // 购买金额因子
   maxBuyAmount: 100, // 最大购买金额
-  startCalcMinDataPoints: 200, // 开始计算的最小数据量
+  startCalcMinDataPoints: 100, // 开始计算的最小数据量
   stopProfitFactor: 0.5, // 止盈因子
   deltaRateThreshold: 0.5, // 价差阈值
   bsmProbThreshold: 0.1, // 概率阈值
