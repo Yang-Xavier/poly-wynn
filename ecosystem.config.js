e0366945c1ad683bfa38278f5e2d58dc3ed998b1
Xavier <xavierybx@gmail.com>
2026-01-03 23:31:52 +0800

    chore: change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
-    // {
-    //   name: "crypto15min-start",
-    //   script: "npm",
-    //   args: "run crypto15min:runPolyWynn",
-    //   cwd: __dirname,
-    //   env: {
-    //     LOGGER_DISABLE_CONSOLE: "true",
-    //     // 将 PASSWORD 传递给子进程
-    //     ...(process.env.PASSWORD && { PASSWORD: process.env.PASSWORD }),
-    //     NODE_ENV: process.env.NODE_ENV || "production",
-    //   },
-    //   out_file: "./logs/pm2-start-out.log",
-    //   error_file: "./logs/pm2-start-error.log",
-    // },
+    {
+      name: "crypto15min-start",
+      script: "npm",
+      args: "run crypto15min:runPolyWynn",
+      cwd: __dirname,
+      env: {
+        LOGGER_DISABLE_CONSOLE: "true",
+        // 将 PASSWORD 传递给子进程
+        ...(process.env.PASSWORD && { PASSWORD: process.env.PASSWORD }),
+        NODE_ENV: process.env.NODE_ENV || "production",
+      },
+      out_file: "./logs/pm2-start-out.log",
+      error_file: "./logs/pm2-start-error.log",
+    },
     {
       name: "server",
       script: "bun",
